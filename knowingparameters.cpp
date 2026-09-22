02b02629f7a16d885d11091c74b1a4aa65f53b57
aekansh-panda <89935603+aekansh-panda@users.noreply.github.com>
2026-09-22 23:47:06 +0530

    Add files via upload
    () iske andar jani wali chizen

---
+++ knowingparameters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void doprint()
+{
+    std::cout<<"In doprint() \n";
+}
+
+void printvalue(int a)
+{
+    std::cout<<a<<'\n';
+}
+
+int add(int x, int y)
+{
+    return x+y;
+}
